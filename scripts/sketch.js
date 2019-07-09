b5b66be777c3444a1e6bf0c3709770c1840415b0
siviwe <xboyfan01>
2019-07-09 02:03:05 -0700

    Object Classifier Added

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -1,12 +1,24 @@
-console.log('ml5 version loaded:', ml5.version);
 
-
-    function setup(){
-        createCanvas(600, 600);
-
-    }
-
-    function draw(){
-        background(0);
-
-    }
+let classifier; let video; let resultsP;
+function setup() {
+  noCanvas();
+  // Create a camera input
+  video = createCapture(VIDEO);
+  // Initialize the Image Classifier method with MobileNet and the video as the second argument
+  classifier = ml5.imageClassifier('MobileNet', video, modelReady);
+  resultsP = createP('Please wait while model is loading....');
+}
+function modelReady() {
+  console.log('Model Ready');
+  classifyVideo(); 
+}
+// Get a prediction for the current video frame
+function classifyVideo() {
+  classifier.classify(gotResult);
+}
+// When we get a result
+function gotResult(err, results) {
+  // The results are in an array ordered by confidence.
+  resultsP.html(results[0].label + ' ' + nf(results[0].confidence, 0, 2));
+  classifyVideo();
+}
